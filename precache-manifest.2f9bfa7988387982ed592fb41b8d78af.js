1a34b16b7384c3960416da540b45181be3aaea4f
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-04 18:42:23 -0300

    Updates

---
+++ precache-manifest.2f9bfa7988387982ed592fb41b8d78af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfc0ded7efd1ef44198e5cc0d1e1c52",
+    "url": "/ci-project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "7778095bba0ff312dcd5",
+    "url": "/ci-project-starwars-planets-search/static/css/main.4c2e10c9.chunk.css"
+  },
+  {
+    "revision": "812c941169c5a73cdc23",
+    "url": "/ci-project-starwars-planets-search/static/js/2.a179908a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ci-project-starwars-planets-search/static/js/2.a179908a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7778095bba0ff312dcd5",
+    "url": "/ci-project-starwars-planets-search/static/js/main.3b26ea32.chunk.js"
+  },
+  {
+    "revision": "7e180267afee2271f103",
+    "url": "/ci-project-starwars-planets-search/static/js/runtime-main.0567b150.js"
+  }
+]);
